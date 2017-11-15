bfa9dc6b88833a834776e776cf35b34396ecce22
HVonck <hvonck@live.nl>
2017-11-15 16:36:57 +0100

    added comma.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $.get(
     "https://www.googleapis.com/youtube/v3/channels", {
       part: 'contentDetails',
-      forUsername: channelName
+      forUsername: channelName,
       key: 'AIzaSyBj4NR2ccKANrB7X85GvG5pcfUyKlWyMus',
       function(data){
         $.each(data.items, function(i, item){
