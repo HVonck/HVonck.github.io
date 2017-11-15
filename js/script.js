5d647eb81012d0775970cfa3ee5e4807dafa7d44
HVonck <hvonck@live.nl>
2017-11-15 16:50:40 +0100

    added getVideos function.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,30 @@ $(document).ready(function(){
       function(data){
         $.each(data.items, function(i, item){
           console.log(item);
+          pId = item.contentDetails.relatedPlaylists.uploads;
+          getVideos(pId);
         })
       }
   );
+
+  function getVideos(pId) {
+    $.get(
+      "https://www.googleapis.com/youtube/v3/playlistItems", {
+        part: 'snippet',
+        maxResults: 10,
+        playlistId = pId,
+        key: 'AIzaSyBj4NR2ccKANrB7X85GvG5pcfUyKlWyMus' },
+        function(data){
+          var output;
+          $.each(data.items, function(i, item){
+            console.log(item);
+            videoTitle = item.snippet.title;
+
+            output = '<li>' + videoTitle + '</li>';
+
+            $('#results').append(output);
+          })
+        }
+    );
+  }
 });
