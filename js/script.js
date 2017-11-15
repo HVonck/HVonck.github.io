88646446cb7fffb5409cab20469bff8896c6ce95
HVonck <hvonck@live.nl>
2017-11-15 16:35:40 +0100

    test iteration.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert(1);
+var channelName = 'Hilze Vonck'
+
+$(document).ready(function(){
+  $.get(
+    "https://www.googleapis.com/youtube/v3/channels", {
+      part: 'contentDetails',
+      forUsername: channelName
+      key: 'AIzaSyBj4NR2ccKANrB7X85GvG5pcfUyKlWyMus',
+      function(data){
+        $.each(data.items, function(i, item){
+          console.log(item);
+        })
+      }
+    }
+  );
+});
