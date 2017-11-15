819747a9222d1d9e0f4cadc080462b283fcea720
HVonck <hvonck@live.nl>
2017-11-15 17:07:39 +0100

    First official working commit. changed channel name to channel id.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var channelName = 'Hilze Vonck';
+var channelId = 'UC8C7ncaMYnXyu-pRU0S9FLg';
 
 $(document).ready(function(){
   $.get(
     "https://www.googleapis.com/youtube/v3/channels", {
       part: 'contentDetails',
-      forUsername: channelName,
+      id: channelId,
       key: 'AIzaSyBj4NR2ccKANrB7X85GvG5pcfUyKlWyMus' },
       function(data){
         $.each(data.items, function(i, item){
